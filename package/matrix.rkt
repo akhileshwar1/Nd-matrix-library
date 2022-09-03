a840a0fae2ecb5e1cfb2e8806cc7d87d043294fc
AKhil <akhilprm999@gmail.com>
2022-09-03 16:39:13 +0530

    tests and functionality in the same file

--- package/matrix.rkt
+++ package/matrix.rkt
@@ -1,30 +1,36 @@
 #lang racket
 ; ---------------------------------------------------------------------------------------------------
 (provide (contract-out 
+           ;; returns a struct matrix with a name and data.
            [matrix (-> list? vector?)]
+           ;; acceses the element/matrix at (pos1 pos2 ...) as dimensions.
            [matrix-ref (->* (vector? list?) vector?)]
+           ;; creating a matrix from a list.
            [list->matrix (->* (list? list?) vector?)]
+           ;; updating the same matrix at a certain position. 
            [matrix-update (->* (vector? list? any/c) void?)]
+           ;; return the dot product of two 1d matrices.
            [matrix-* (->* (vector? vector?) vector?)]))
 
 ; ---------------------------------------------------------------------------------------------------
 (require rackunit)
+(require rackunit/text-ui)
 ; ---------------------------------------------------------------------------------------------------
 ; BODY
 
+;(get-dims (vector (vector 0 0 0) (vector 0 0 0)))
+
 ;; get the dims of an input matrix.
-;; matrix -> (list of dim)
 (define (get-dims m)
   (cond [(not (vector? m)) '()]
         [else (cons (vector-length m) (get-dims (vector-ref m 0)))]))
 
-;(get-dims (vector (vector 0 0 0) (vector 0 0 0)))
 
 
 ;(struct Matrix (name dims data) #:transparent)
 
-;; returns a struct matrix with a name and data.
-;; string, number ... -> matrix.
+;(define m (matrix  (list 3 3 3)))
+
 (define (matrix x)
   (cond [(equal? 1 (car x)) (matrix (cdr x))] 
         [(equal? 1 (length x))  (build-vector (car x) (lambda (x) 0))]
@@ -32,11 +38,7 @@
           (build-vector (car x) (lambda (n) (matrix (cdr x))))])) 
 
 
-;(define m (matrix  (list 3 3 3)))
-;(displayln m)
 
-;; acceses the element/matrix at (pos1 pos2 ...) as dimensions.
-;; matrix, number, number ... -> matrix.
 (define (matrix-ref m xpos)
   (define dims (get-dims m))
   (let* ([c-dim (if (empty? dims) 0 (car dims))] [r-dim (if (empty? dims) 0 (cdr dims))] 
@@ -55,7 +57,6 @@
 
 
 ;; splicing matrix for special cases like all or a range.
-;; matrix, dims, pair, list => matrix. 
 (define (get-vector-in-range m dims range xpos)
   (define next-slice (cdr xpos))
   (define start (car range))
@@ -65,28 +66,17 @@
                   (matrix-ref  (vector-ref m i) (cdr xpos)))))
 
 
-;(define n (matrix  (list 3 3)))
-;(Matrix-data n)
-;(define ref (matrix-ref n (list "A" 0)))
-;(displayln ref)
-;(define ref1 (matrix-ref n (list (cons 1 2) 0)))
-;(displayln ref1)
+;(list->matrix (list (list 1 2 3) (list 4 5 6) (list 7 8 9)) (list 3 3)) 
+;(list->matrix (list (list (list 1 2 3) (list 4 5 6) (list 7 8 9)) (list (list 1 2 3) (list 4 5 6) (list 7 8 9))) (list 2 3 3)) 
 
-
-;; creating a matrix from a list.
-;; list, name -> matrix
 (define (list->matrix lt dims)
   (let* ([c-dim (if (empty? dims) 0 (car dims))] [r-dim (if (empty? dims) 0 (cdr dims))])
     (cond [(equal? (length dims) 1) (list->vector lt)]
           [else (list->vector (for/list ([i (in-range c-dim)])
                                 (list->matrix (list-ref lt i) r-dim)))])))
 
-;(list->matrix (list (list 1 2 3) (list 4 5 6) (list 7 8 9)) (list 3 3)) 
-;(list->matrix (list (list (list 1 2 3) (list 4 5 6) (list 7 8 9)) (list (list 1 2 3) (list 4 5 6) (list 7 8 9))) (list 2 3 3)) 
+;(matrix-update n (list 1 2) 3)
 
-
-;; updating the same matrix at a certain position. 
-;; matrix, pos -> matrix.
 (define (matrix-update m posn element)
   (let* ([c-pos (if (empty? posn) posn (car posn))] [r-pos (if (empty? posn) posn (cdr posn))])
     (cond [(empty? r-pos)  
@@ -96,12 +86,9 @@
 
 
 
-;(matrix-update n (list 1 2) 3)
-;n
 
+;(dot-product (matrix (list 5)) (matrix (list 4)))
 
-;; return the dot product of two 1d matrices.
-;; matrix, matrix -> vector/Od 
 (define (dot-product m1 m2)
   (define m1-dims (get-dims m1))
   (define m2-dims (get-dims m2))
@@ -112,29 +99,23 @@
      [j (vector->list m2)])
     (+ result (* i j))))
 
-;; START
-#|(cond [(equal? ln-m1 ln-m2) (dot m1-data m2-data)]
-        [else (displayln "they have to be of same length")])|#;)
 
-;(dot-product (matrix (list 5)) (matrix (list 4)))
-
-
-;; returning transpose of a 2d matrix.
-;; matrix -> transposed matrix. 
-(define (2d-transpose m)
-  (define dims (get-dims m))
-  (build-vector (car dims) (lambda (n)
-                             (matrix-ref m (list "A" n)))))
-#|(cond [(equal? (length dims) 2) (Matrix "res" (list (cdr dims) (car dims)) (transpose data))]
-        [else "it has to be a 2d matrix"])|#
 
 ;(define ex (list->matrix (list (list 1 2 3) (list 1 2 3) (list 1 2 3)) (list 3 3)))
 ;(define ex1 (list->matrix (list (list 1 2 3) (list 1 2 3)) (list 2 3)))
 ;(2d-transpose ex1)
 
+;; returning transpose of a 2d matrix.
+(define (2d-transpose m)
+  (define dims (get-dims m))
+  (build-vector (car dims) (lambda (n)
+                             (matrix-ref m (list "A" n)))))
+
+
+;(define ex2 (list->matrix (list (list 1 1 1) (list 1 2 3) (list 1 2 1)) (list 3 3)))
+;(matrix-* ex  ex2)
 
 ;; multiplying two 2d matrices.
-;; matrix, matrix -> vector.
 (define (matrix-* m1 m2)
   (define m2-new (2d-transpose m2))
   (define m1-ln (vector-length m1))
@@ -146,15 +127,43 @@
                                         (define ls2 (matrix-ref m2-new (list n2)))
                                         (dot-product ls1 ls2))))))
 
-;(define ex2 (list->matrix (list (list 1 1 1) (list 1 2 3) (list 1 2 1)) (list 3 3)))
-;(matrix-* ex  ex2)
 
 
 ; ---------------------------------------------------------------------------------------------------
 ;; TESTING
-#|(define tests
-    (test-suite
-      ""
+
+(module+ test
+(define tests
+  (test-suite
+    "matrix tests"
+    (test-case 
+      "cases for matrix function"
       (test-begin
-        "
-        (check-equal? ))))|#
+        (check-equal? (matrix '(3 3)) (vector (vector 0 0 0) (vector 0 0 0) (vector 0 0 0)))
+        (check-equal? (matrix "x" ) (vector 0 0 0))))
+
+
+    (test-case 
+      "cases for matrix-ref function"
+      (test-begin
+        (define m (matrix '(3 3)))
+        (check-equal? (matrix-ref m (list "A" 0))  (vector 0 0 0))
+        (check-equal? (matrix-ref m (list (cons 0 2) 0)) (vector 0 0))
+        (check-equal? (matrix-ref m (list "A" "A"))  (vector (vector 0 0 0) (vector 0 0 0) 
+                                                             (vector 0 0 0)))))
+
+    (test-case 
+      "cases for matrix-update function"
+      (test-begin
+        (define m (list->matrix  (list (list 1 2 3) (list 4 5 6) (list 7 8 9)) (list 3 3)))
+        (matrix-update m (list 1 2) "a")
+        (check-equal?  m (vector (vector 1 2 3) (vector 4 5  "a")
+                                 (vector 7 8 9)))
+        (matrix-update m (list 1) (vector "x" "x" "x"))
+        (check-equal?  m (vector (vector 1 2 3)
+                                 (vector "x" "x" "x")
+                                 (vector 7 8 9)))))
+    ))
+
+(run-tests tests))
+
